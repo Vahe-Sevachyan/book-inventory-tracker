de6de49eb82fb437d155540f5bce05b2bdb9b0a6
Vahe Sevachyan <vscode13@gmail.com>
2023-06-11 18:53:43 -0700

    added book add button fucntionality

--- Backup/other/index.js
+++ Backup/other/index.js
@@ -1,5 +1,3 @@
-const modal = document.getElementById("bookModal");
-const bookInfoDiv = document.getElementById("bookInfo");
 class Book {
   constructor(title, author, publicationYear, genre) {
     this.title = title;
@@ -29,6 +27,10 @@ class Book {
   }
 }
 
+const modal = document.getElementById("bookModal");
+const displayInfo = document.getElementById("displayInfo");
+let book;
+
 function openModal() {
   modal.style.display = "block";
 }
@@ -36,33 +38,18 @@ function openModal() {
 function closeModal() {
   modal.style.display = "none";
 }
-// modal.addEventListener("click", openModal());
-// function addRemoveBtn() {
-//   const btn = document.createElement("BUTTON");
-//   btn.innerHTML = "Delete";
-//   bookInfoDiv.appendChild(btn);
-//   //
-// }
 
-// removeBtn.forEach((btn) => {
-//   btn.addEventListener("click", removeBookBtn());
-// });
-
-function removeBookBtn() {
-  bookInfoDiv.innerHTML = "";
-  book = null;
-}
-
-function addBook() {
+function saveBook() {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const publicationYear = document.getElementById("publicationYear").value;
   const genre = document.getElementById("genre").value;
-  const book = new Book(title, author, publicationYear, genre);
-  bookInfoDiv.innerHTML += book.getBookInfo() + "<hr>";
-  const removeBtn = document.createElement("BUTTON");
-  removeBtn.setAttribute("ID", "removeBtn");
-  removeBtn.innerHTML = "Delete";
-  bookInfoDiv.appendChild(removeBtn);
-  closeModal();
+
+  book = new Book(title, author, publicationYear, genre);
+  displayInfo.innerHTML = book.getBookInfo();
+}
+
+function removeBook() {
+  displayInfo.innerHTML = "";
+  book = null;
 }
--- Backup/script.js
+++ Backup/script.js
@@ -1,5 +1,4 @@
 const modal = document.getElementById("bookModal");
-const bookInfoDiv = document.getElementById("bookInfo");
 class Book {
   constructor(title, author, publicationYear, genre) {
     this.title = title;
@@ -36,33 +35,19 @@ function openModal() {
 function closeModal() {
   modal.style.display = "none";
 }
-// modal.addEventListener("click", openModal());
-// function addRemoveBtn() {
-//   const btn = document.createElement("BUTTON");
-//   btn.innerHTML = "Delete";
-//   bookInfoDiv.appendChild(btn);
-//   //
-// }
-
-// removeBtn.forEach((btn) => {
-//   btn.addEventListener("click", removeBookBtn());
-// });
-
-function removeBookBtn() {
-  bookInfoDiv.innerHTML = "";
-  book = null;
-}
 
 function addBook() {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const publicationYear = document.getElementById("publicationYear").value;
   const genre = document.getElementById("genre").value;
+
   const book = new Book(title, author, publicationYear, genre);
+
+  const bookInfoDiv = document.getElementById("bookInfo");
+
+  const button = document.createElement("button");
+  button.innerText = "Can you click me?";
   bookInfoDiv.innerHTML += book.getBookInfo() + "<hr>";
-  const removeBtn = document.createElement("BUTTON");
-  removeBtn.setAttribute("ID", "removeBtn");
-  removeBtn.innerHTML = "Delete";
-  bookInfoDiv.appendChild(removeBtn);
   closeModal();
 }
