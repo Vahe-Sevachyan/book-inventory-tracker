824c8bab62c9e94d1ce8b3df49ed9eb692f43cb2
Vahe Sevachyan <vscode13@gmail.com>
2023-06-28 21:27:33 -0700

    added savebook function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const createBook = document.querySelector(".createBook");
 const modal = document.querySelector("#bookModal");
 const saveBtn = document.querySelector(".saveBtn");
 const closeBtn = document.querySelector(".closeBtn");
+const bookList = document.querySelector(".bookList");
 let books = [];
 
 createBook.onclick = () => {
@@ -10,9 +11,35 @@ createBook.onclick = () => {
 closeBtn.onclick = () => {
   closeModal();
 };
+saveBtn.onclick = () => {
+  saveBook();
+};
+
 function openModal() {
   modal.style.display = "block";
 }
 function closeModal() {
   modal.style.display = "none";
+}
+
+class Book {
+  title;
+  author;
+  publicationYear;
+  genre;
+  getTile() {
+    return title;
+  }
+  getAuthor() {
+    return author;
+  }
+  getPublicationYear() {
+    return publicationYear;
+  }
+  getGenre() {
+    return genre;
+  }
+  getBookInfo() {
+    return `Title:${title}<br>Author:${author}<br> Publication Year:${publicationYear}<br> Genre:${genre}`;
+  }
 }
