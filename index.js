17a6123aba2ebe8f6df0846f65fdc2b7fc103fea
Vahe Sevachyan <vscode13@gmail.com>
2023-11-26 20:11:48 -0800

    implemented Book constructor function

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const closeBtn = document.querySelector(".close-btn");
 
 const myLibrary = [];
 
-function Book() {}
-
 function addBookToLibrary() {}
 
 addBookBtn.onclick = () => {
@@ -14,3 +12,15 @@ addBookBtn.onclick = () => {
 closeBtn.onclick = () => {
   modal.style.display = "none";
 };
+
+function Book(title, author, publicationYear, genre) {
+  this.title = title;
+  this.author = author;
+  this.publicationYear = publicationYear;
+  this.genre = genre;
+  this.getInfo = function () {
+    return `Title: ${this.title} Author:${this.author} Publication Year: ${this.publicationYear} Genre: ${this.genre}`;
+  };
+}
+const newBook = new Book("atomic habits", "James Clear", 2012, "kaizen");
+console.log(newBook);
