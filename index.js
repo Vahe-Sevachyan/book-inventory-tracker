067a0b6ef59cbdb6edf236c6d0cd69cd434dab11
Vahe Sevachyan <vscode13@gmail.com>
2023-11-27 20:10:00 -0800

    added saveBook function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const addBookBtn = document.querySelector(".create-book-btn");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close-btn");
-
+const saveBtn = document.querySelector(".save-btn");
 const myLibrary = [];
 
 function addBookToLibrary() {}
@@ -13,6 +13,9 @@ closeBtn.onclick = () => {
   modal.style.display = "none";
 };
 
+saveBtn.onclick = () => {
+  saveBook();
+};
 function Book(title, author, publicationYear, genre) {
   this.title = title;
   this.author = author;
@@ -24,3 +27,17 @@ function Book(title, author, publicationYear, genre) {
 }
 const newBook = new Book("atomic habits", "James Clear", 2012, "kaizen");
 console.log(newBook);
+
+//next work on the functionality to save the information to the array
+// take the info from the input element and pass it to the book constructor save it in a variable maybe and push it to the array
+
+function saveBook() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const publicationYear = document.getElementById("publicationYear").value;
+  const genre = document.getElementById("genre").value;
+
+  const newBook = new Book(title, author, publicationYear, genre);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+}
